17847e1f33362bff537a9decc294089cb15ded87
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2019-12-26 19:08:38 +0545

    Login Activity Completed.

--- app/src/main/java/com/deepak/esoftwarica/LoginActivity.java
+++ app/src/main/java/com/deepak/esoftwarica/LoginActivity.java
@@ -2,13 +2,59 @@ package com.deepak.esoftwarica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (validate()) {
+                    if ((username.equals("softwarica") && (password.equals("coventry")))) {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    private boolean validate(){
+        if (TextUtils.isEmpty(etUsername.getText())){
+            etUsername.setError("Enter username");
+            etUsername.requestFocus();
+            return false;
+        }else if (TextUtils.isEmpty(etPassword.getText())){
+            etPassword.setError("Enter password");
+            etPassword.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
