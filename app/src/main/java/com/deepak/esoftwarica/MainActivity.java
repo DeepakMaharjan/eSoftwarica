58e8849824bf8b90ab7b910036cb61ec52024826
DeepakMaharjan <Deepakmaharjan10@gmail.com>
2019-12-26 20:16:03 +0545

    Addded Adapters for configure the students data.

--- app/src/main/java/com/deepak/esoftwarica/MainActivity.java
+++ app/src/main/java/com/deepak/esoftwarica/MainActivity.java
@@ -1,14 +1,57 @@
 package com.deepak.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.deepak.esoftwarica.adapter.Students;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static List<Students> studentsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemReselectedListener(navListener);
+
+        if (studentsList.isEmpty()){
+            studentsList.add(new Students("Deepak Maharjan","male", "Sanagaun, Lalitpur", 18));
+            studentsList.add(new Students("Sita Shrestha", "female", "Tinkune, Kathmandu", 20));
+            studentsList.add(new Students("Test", "other", "Gaushala, Kathmandu", 21));
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.navHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navAddStudent:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navAboutUs:
+                    selectedFragment = new HomeFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/deepak/esoftwarica/adapter/StudentsAdapter.java
@@ -0,0 +1,13 @@
+package com.deepak.esoftwarica.adapter;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class StudentsAdapter {
+    Context context;
+    List<Students> studentsList;
+
+
+
+}
